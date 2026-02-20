32d9506fac2fb22a16d676b5b8d5c085d4153460
amehdmendez <amehdmendez@gmail.com>
2026-02-20 17:01:18 +0000

    mask

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('app-ads.txt', RedirectView.as_view(url='/static/app-ads.txt')),
     path('', include('web.urls')),
 ]
