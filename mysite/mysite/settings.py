82824aa8022c00b1a6020daa04031a1ee1c299d4
amehdmendez <amehdmendez@gmail.com>
2026-02-20 15:14:35 +0000

    cambios

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^=6-_k)oh!n9-fpcd1qd0rf(!8y2!!8cc*so1if(!*ydv@*_dc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dsem.app', 'helfy.dsem.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
