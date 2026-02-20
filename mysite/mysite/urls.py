8013bf1b464ce54e4e545cc512b0305ce0b8dde7
amehdmendez <amehdmendez@gmail.com>
2026-02-20 17:05:11 +0000

    nsofins

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app-ads.txt', RedirectView.as_view(url='/static/app-ads.txt')),
     path('', include('web.urls')),
+    path('app-ads.txt', RedirectView.as_view(url='/static/app-ads.txt')),
 ]
+
+# Servir archivos estáticos durante el desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
